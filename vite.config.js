cd2d05f7ef5d8611feeb6e352294da6f615fe5e7
Dhayanand458 <141307826+Dhayanand458@users.noreply.github.com>
2024-12-11 23:28:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import glsl from 'vite-plugin-glsl'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), glsl()],
-})
+  base: '/Dhayanand458/', // Replace <repository-name> with your repository name
+  build: {
+    outDir: 'dist',
+  },
+});
